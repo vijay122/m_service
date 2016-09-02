506f2b86c9f03820c00eadaba472026507f00660
vijay122 <raagavan.vijay@gmail.com>
2016-09-02 21:36:27 +0530

    configure online mongolab db

--- routes/places.js
+++ routes/places.js
@@ -7,7 +7,11 @@ var Async = require('async');
 var Server = mongo.Server,
     Db = mongo.Db,
     BSON = mongo.BSONPure;
-var server = new Server('localhost', 27017, {auto_reconnect: true});
+//var server = new Server('localhost', 27017, {auto_reconnect: true});
+
+var mongoconnection = "mongodb://user:password@ds019916.mlab.com:19916/heroku_wls18qcv";
+
+server = new Server(mongoconnection,{auto_reconnect:true});
 var GoogleMapsAPI = require('googlemaps');
 var forecast = new Forecast({
     service: 'forecast.io',
